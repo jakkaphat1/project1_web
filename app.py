24c299bd3462d23a2a9160d8060daeb7cded7ce8
jakkaphat1 <jakkaphat.w@kkumail.com>
2025-09-11 01:42:45 +0700

    fix word eng and thai

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def process_and_compare(uploaded_files, processing_mode, use_template, template_
         st.session_state.processed_filenames = {
             d["doc_id"]: d["filename"] for d in processed_data.values()}
 
-        status_text.text("✅ Done!")
+        status_text.text("ประมวลผลเสร็จสิ้น")
         st.success("Processing, comparison, and clustering complete!")
         st.balloons()
     except Exception as e:
